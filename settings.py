1ea11a8c367c840476e7f21a1fcd0711862f5576
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-06-24 20:27:22 -0400

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+import os
+BASE = os.path.dirname(os.path.realpath(__file__))
+STATIC_PATH = os.path.join(BASE, 'static')
+TEMPLATE_PATH = os.path.join(BASE, 'templates')
+PLAYLIST_DIR = os.path.join(BASE, "Playlist/")
+MUSIC_DIR = os.path.join(BASE, "Music/")
+#Path to Binaries
+FFMPEG = ''
